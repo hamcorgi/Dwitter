a98ffdb5755068d1fd1746a839a11cfde9c8c3b8
Auntony Mika <kotyu88@gmail.com>
2024-04-11 16:01:10 +0900

    Dwitter

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ const AUTH_ERROR = { messsage: 'Authentication Error'};
 
 export const isAuth = async (req, res, next) => {
     const authHeader = req.get('Authorization');
-    if(!(authHeader && authHeader.startsWith('Bearer'))){
+    if(!(authHeader && authHeader.startsWith('Bearer '))){
         return res.status(401).json(AUTH_ERROR);
     }
     
@@ -17,11 +17,11 @@ export const isAuth = async (req, res, next) => {
         'F2dN7x8HVzBWaQuEEDnhsvHXRWqAR63z',
         async (error, decoded) => {
             if(error){
-                return res.status(401).json(AUTH_ERROR);
+                return res.status(401).json({message: "Error"});
             }
             const user = await userRepository.findById(decoded.id);
             if(!user){
-                return res.status(401).json(AUTH_ERROR);
+                return res.status(401).json({message: "not found user"});
             }
             req.userId = user.id; //req.customData
             next();
